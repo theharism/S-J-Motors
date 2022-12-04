f785171e64f2ed430465702c725fe68ee9ada0c6
theharism <85572852+theharism@users.noreply.github.com>
2022-12-04 17:16:48 +0500

    Add files via upload
    Added Database Connectivity

---
+++ src/main/java/com/mycompany/s/jmotors/Expenditure.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.s.jmotors;
+
+/**
+ *
+ * @author city
+ */
+public class Expenditure {
+    
+    float amount;
+    String date;
+    
+    public boolean addNewExpense(){return true;};
+    public boolean updateExpense(){return true;};
+    public void displayExpense(){};
+}
